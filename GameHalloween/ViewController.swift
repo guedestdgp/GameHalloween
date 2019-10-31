1d6e4fe9450a06eeb4eba029d2b01d1ccf6426ff
Tatiane <guedestdgp@outlook.com>
2019-10-31 13:59:45 -0400

    Layout finished Created dictionary with emojis

--- GameHalloween/ViewController.swift
+++ GameHalloween/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbLives: UILabel!
+    var seconds = 5
+    var timer: Timer?
+    var isTimerRunning = false
+    var stuff = [(String, String)]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stuff = [("Dog", "🐶"), ("Cat", "🐱"), ("Mice", "🐭"), ("Fox", "🦊"), ("Rooster", "🐔"), ("Panda", "🐼"), ("Pumpkin", "🎃"), ("Ghost", "👻"), ("Dice", "🎲"), ("Game", "🎳")]
+        lbLives.text = "Lives: 10"
     }
 
 
