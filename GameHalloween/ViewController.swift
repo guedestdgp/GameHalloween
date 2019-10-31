90660445942ab18fdb6eb579afa02fc8762d7a20
Tatiane <guedestdgp@outlook.com>
2019-10-31 14:30:24 -0400

    Created random and timer.

--- GameHalloween/ViewController.swift
+++ GameHalloween/ViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var btsChoices: [UIButton]!
     @IBOutlet weak var lbLives: UILabel!
+    @IBOutlet weak var btStart: UIButton!
+    @IBOutlet weak var lbTimer: UILabel!
+    @IBOutlet weak var lbPoint: UILabel!
+    @IBOutlet weak var lbImages: UILabel!
     var seconds = 5
     var timer: Timer?
     var isTimerRunning = false
@@ -20,8 +25,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         stuff = [("Dog", "🐶"), ("Cat", "🐱"), ("Mice", "🐭"), ("Fox", "🦊"), ("Rooster", "🐔"), ("Panda", "🐼"), ("Pumpkin", "🎃"), ("Ghost", "👻"), ("Dice", "🎲"), ("Game", "🎳")]
         lbLives.text = "Lives: 10"
+        lbPoint.text = "Point: 0"
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        lbImages.text = stuff.randomElement()!.1
     }
 
 
+    @IBAction func btsChoicesAction(_ sender: UIButton) {
+    }
+    
+    @objc func update() {
+        lbImages.text = stuff.randomElement()!.1
+        //To stop: timer.invalidate()
+    }
+    
+    func randonImages() -> String {
+        return stuff.randomElement()!.1
+    }
 }
 
