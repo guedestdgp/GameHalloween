2ef21b4ff61a7aed0cf008a4b1a2d11a8043bcbc
Tatiane <guedestdgp@outlook.com>
2019-10-31 14:56:05 -0400

    Random for buttons

--- GameHalloween/ViewController.swift
+++ GameHalloween/ViewController.swift
@@ -16,8 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbTimer: UILabel!
     @IBOutlet weak var lbPoint: UILabel!
     @IBOutlet weak var lbImages: UILabel!
-    var seconds = 5
+    var seconds = 60
     var timer: Timer?
+    var timerCount: Timer?
+    var timerButtons: Timer?
     var isTimerRunning = false
     var stuff = [(String, String)]()
     override func viewDidLoad() {
@@ -26,8 +28,12 @@ class ViewController: UIViewController {
         stuff = [("Dog", "🐶"), ("Cat", "🐱"), ("Mice", "🐭"), ("Fox", "🦊"), ("Rooster", "🐔"), ("Panda", "🐼"), ("Pumpkin", "🎃"), ("Ghost", "👻"), ("Dice", "🎲"), ("Game", "🎳")]
         lbLives.text = "Lives: 10"
         lbPoint.text = "Point: 0"
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
-        lbImages.text = stuff.randomElement()!.1
+        lbTimer.text = "\(seconds)"
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        timerCount = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countTimer), userInfo: nil, repeats: true)
+        timerButtons = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(buttonsChange), userInfo: nil, repeats: true)
+        update()
+        buttonsChange()
     }
 
 
@@ -37,6 +43,22 @@ class ViewController: UIViewController {
     @objc func update() {
         lbImages.text = stuff.randomElement()!.1
         //To stop: timer.invalidate()
+    }
+    
+    @objc func buttonsChange() {
+        btsChoices[0].setTitle(stuff.randomElement()!.0, for: .normal)
+        btsChoices[1].setTitle(stuff.randomElement()!.0, for: .normal)
+        btsChoices[2].setTitle(stuff.randomElement()!.0, for: .normal)
+        btsChoices[3].setTitle(stuff.randomElement()!.0, for: .normal)
+    }
+    
+    @objc func countTimer() {
+        let getTimer = Int(lbTimer.text!)
+        if getTimer != 0 { lbTimer.text = "\(getTimer! - 1)" }
+        else {
+            timer?.invalidate()
+            timerCount?.invalidate()
+        }
     }
     
     func randonImages() -> String {
